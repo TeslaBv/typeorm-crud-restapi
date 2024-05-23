264245c5f8affed84fe12b56975b59f5ce27418c
Fabian <cruzfabianhiram@gmail.com>
2024-05-22 18:25:17 -0600

    Commit Inicial

---
+++ src/db.ts
@@ -0,0 +1,31 @@
+import { DataSource } from "typeorm";
+import {User} from '../src/entity/User';
+import { Empleado } from "./entity/empleado";
+import { Area } from "./entity/Area";
+import { Edificio } from "./entity/Edificio";
+import { Vale } from "./entity/vale";
+
+
+/*  export const AppDataSourse = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    username: 'administrador',
+    password: 'administrador',
+    port: 5432,
+    database: 'db_sice',
+    entities: [User,Empleado, Edificio, Area, Vale],
+    logging: true,
+    synchronize: true
+})*/
+
+export const AppDataSourse = new DataSource({
+    type: 'postgres',
+    host: 'viaduct.proxy.rlwy.net',
+    username: 'postgres',
+    password: 'cXPPURdYueBeGPdFHJdpMlpsVylbInjz',
+    port: 57187,
+    database: 'railway',
+    entities: [User,Empleado, Edificio, Area, Vale],
+    logging: true,
+    synchronize: true
+})
